a657e10c4412fe3bfc7e7183f111c61790eadb6f
Angela <angelayh.liu@gmail.com>
2017-02-25 09:08:38 -0500

    create midter project

---
+++ README.md
@@ -0,0 +1,5 @@
+# COMP308-W2017-MidTerm-300714839
+
+## Welcome to the MidTerm Project - the Favourite Book List App
+
+please use **`npm install`** to install project dependencies
---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+//index.js Angela Liu 300714839 COMP308-W2017-MIDTERMPROJECT-Favourite	Book	List
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the game model
+let book = require('../models/books');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    books: ''
+   });
+});
+
+module.exports = router;
